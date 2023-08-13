d220912a70009b343f27c8ac28542b9f7ea82ca2
permadihendra <26792223+permadihendra@users.noreply.github.com>
2023-08-14 06:07:38 +0700

    Configure fastapi docker

---
+++ app/main.py
@@ -0,0 +1,20 @@
+from typing import Union
+
+from fastapi import FastAPI
+
+app = FastAPI()
+
+
+@app.get("/")
+def read_root():
+    return {"Hello My": "Fantastic World"}
+
+
+@app.get("/items/{item_id}")
+def read_item(item_id: int, q: Union[str, None] = None):
+    return {"item_id": item_id, "q": q}
+
+
+@app.get("/category/{category_id}")
+def read_category(category_id: int, q: Union[str, None] = None):
+    return {"category_id": category_id, "q": q}
